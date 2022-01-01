2deddd279acc07a54689a604a89b574c64e2ae3a
Jason Tran <jjsontran@gmail.com>
2021-12-31 22:54:02 -0500

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
-import './App.scss';
+import "./App.scss";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
-
 function App() {
-  
   const [listOfThoughts, setListOfThoughts] = useState([]);
   const [title, setTitle] = useState("");
   const [text, setText] = useState("");
   const [createdat, setCreatedAt] = useState("");
 
-  const [createMenu, setCreateMenu] 
+  const [createMenu, setCreateMenu] = useState(false);
+  const [createPrompt, setCreatePrompt] = useState(false);
 
-  
   useEffect(() => {
     Axios.get("http://localhost:3001/getThoughts").then((response) => {
       setListOfThoughts(response.data);
@@ -35,43 +33,47 @@ function App() {
       ]);
     });
   };
-  
+
   return (
     <div className="App">
-      <div className="thoughtsDisplay">
-        {listOfThoughts.map((thought) => {
-          return (
-            <div>
-              <h1>Title: {thought.title}</h1>
-              <h1>Text: {thought.text}</h1>
-            </div>
-          );
-        })}
+      <div className="header">Void</div>
+      <div className="content">
+        <div className="thoughtsDisplay">
+          {listOfThoughts.map((thought) => {
+            return (
+              <div>
+                <h1>{thought.title}</h1>
+                <h1>{thought.text}</h1>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className={"createPrompt " + (createMenu && "active")}>
+          <input
+            name="title"
+            type="text"
+            placeholder="Title"
+            onChange={(event) => {
+              setTitle(event.target.value);
+            }}
+          />
+          <textarea
+            name="body"
+            cols="50"
+            rows="5"
+            onChange={(event) => {
+              setText(event.target.value);
+            }}
+          />
+          <button onClick={createThoughts}> Create Thought </button>
+        </div>
       </div>
 
-      <div>
-        <input
-          type="text"
-          placeholder="Title..."
-          onChange={(event) => {
-            setTitle(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Text..."
-          onChange={(event) => {
-            setText(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Timestsamp..."
-          onChange={(event) => {
-            setCreatedAt(event.target.value);
-          }}
-        />
-        <button onClick={createThoughts}> Create Thought </button>
+      <div className={"createButton " + (createMenu && "active")}>
+        <div className="button" onClick={() => setCreateMenu(!createMenu)}>
+          +
+        </div>
       </div>
     </div>
   );
